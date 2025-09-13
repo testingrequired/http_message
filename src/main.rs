23c7d0d9cd8637d7ae0c47dec17c9421f04b8291
Kylee Tilley <kyleetilley@gmail.com>
2025-09-12 22:02:39 -0500

    Add very basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+    use std::env;
+    use std::fs;
+
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Usage: {} <file_path>", args[0]);
+        std::process::exit(1);
+    }
+
+    match fs::read_to_string(&args[1]) {
+        Ok(contents) => {
+            let partial = http_message::PartialHttpRequest::from_str(&contents);
+
+            println!("{partial:#?}");
+        }
+        Err(e) => eprintln!("Error reading file {}: {}", args[1], e),
+    }
 }
